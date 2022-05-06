84ca4ec08f5195b10e5f51177803c7286fbd084a
Chiragmodi01 <chiragmodi2001@gmail.com>
2022-05-06 06:56:39 +0530

    auth and 404 page

--- src/comps/Sidebar/Sidebar.jsx
+++ src/comps/Sidebar/Sidebar.jsx
@@ -3,15 +3,22 @@ import './Sidebar.css'
 
 import {AiFillHome, AiOutlineHome, AiFillClockCircle, AiOutlineClockCircle, AiOutlineHistory, RiThumbUpFill, RiThumbUpLine, MdVideoLibrary, MdOutlineVideoLibrary} from '../../utils/getIcons';
 import { useMain } from '../../helpers/context/main-context';
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
 
 function Sidebar() {
-  const { hideMenu, onMobile } = useMain();
+  const { hideMenu, onMobile, userLoggedIn} = useMain();
+  let navigate = useNavigate();
   const {pathname} = useLocation();
   const IconHome = pathname === "/" ? AiFillHome : AiOutlineHome;
   const IconPlaylist = pathname === "/playlist" ? MdVideoLibrary : MdOutlineVideoLibrary;
   const IconWatchlater = pathname === "/watchlater" ? AiFillClockCircle : AiOutlineClockCircle;
   const IconLike = pathname === "/liked" ? RiThumbUpFill : RiThumbUpLine;
+
+  const navigateToSignup = () => {
+    setTimeout(() => {
+      !userLoggedIn && navigate("/signup")
+    }, 0)
+  }
 
   return (
     <div className={`Sidebar ${hideMenu && 'short'} ${onMobile && 'tiny'}`}>
@@ -20,19 +27,19 @@ function Sidebar() {
           <IconHome className='menu-icon' size='1.5em'/>
           <h4 className="sidebar-menu-title">Home</h4>
         </NavLink>
-        <NavLink to="/playlist" className={hideMenu ? "sidebar-menu short cursor-pointer" : " cursor-pointer sidebar-menu flex"}>
+        <NavLink onClick={navigateToSignup} to="/playlist" className={hideMenu ? "sidebar-menu short cursor-pointer" : " cursor-pointer sidebar-menu flex"}>
           <IconPlaylist className='menu-icon' size='1.5em'/>
           <h4 className="sidebar-menu-title">Playlists</h4>
         </NavLink>
-        <NavLink to="/watchlater" className={hideMenu ? "sidebar-menu short cursor-pointer" : " cursor-pointer sidebar-menu flex"}>
+        <NavLink onClick={navigateToSignup} to="/watchlater" className={hideMenu ? "sidebar-menu short cursor-pointer" : " cursor-pointer sidebar-menu flex"}>
           <IconWatchlater className='menu-icon' size='1.5em'/>
           <h4 className="sidebar-menu-title">Watch Later</h4>
         </NavLink>
-        <NavLink to="/liked" className={hideMenu ? "sidebar-menu short cursor-pointer" : " cursor-pointer sidebar-menu flex"}>
+        <NavLink onClick={navigateToSignup} to="/liked" className={hideMenu ? "sidebar-menu short cursor-pointer" : " cursor-pointer sidebar-menu flex"}>
           <IconLike className='menu-icon' size='1.5em'/>
           <h4 className="sidebar-menu-title">Liked</h4>
         </NavLink>
-        <NavLink to="/history" className={hideMenu ? "sidebar-menu short cursor-pointer" : " cursor-pointer sidebar-menu flex"}>
+        <NavLink onClick={navigateToSignup} to="/history" className={hideMenu ? "sidebar-menu short cursor-pointer" : " cursor-pointer sidebar-menu flex"}>
           <AiOutlineHistory className={pathname === "/history" ? 'menu-icon active-history' : 'menu-icon'} size='1.5em'/>
           <h4 className="sidebar-menu-title">History</h4>
         </NavLink>
---
+++ src/comps/Toast/Toast.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { ToastContainer, Slide, Zoom, Flip, Bounce } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {useMain} from '../../helpers/context/main-context';
+
+function Toast() {
+    const {lightTheme, toastDelay} = useMain();
+
+  return (
+     <ToastContainer
+        position="bottom-right"
+        autoClose={toastDelay}
+        hideProgressBar={false}
+        transition={Bounce}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        limit={2}
+        theme='theme'
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        toastClassName={lightTheme ? "light-toast" : "dark-toast"}
+    />
+  )
+}
+
+export default Toast
