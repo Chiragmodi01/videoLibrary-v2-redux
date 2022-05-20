e36137750d16446da90c8feba3427b8ef3fc7b43
Chiragmodi01 <chiragmodi2001@gmail.com>
2022-05-20 21:41:42 +0530

    update: db

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,19 +5,19 @@
 
  export const videos = [
   {
-    _id: "KRvv0QdruMQ",
-    title: "Are There Lost Alien Civilizations in Our Past?",
+    _id: "g87ZSTomZDY",
+    title: "Vitalik: Ethereum, Part 1",
     description:
-      "When we think about alien civilizations we tend to look into the vastness of space, to far away planets. But there is another incredibly vast dimension that we might be giving too little thought to: time.",
-    channel: "Kurzgesagt",
-    timesAgo: "Mar 1, 2022",
-    timeLength: "11:56",
-    channelJoined: "Jul 10, 2013",
-    channelViews: "1,265,142",
-    views: "8.7M views",
-    subscribers: "18.5M",
-    channelImg: "AKedOLRVT94BfVl1Xw1UCqakso5v7sSWB0QQvLd3fAAKTw",
-    category: 'Kurzgesagt'
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    channel: "Naval",
+    timesAgo: "5 hours ago",
+    timeLength: "0:16",
+    channelJoined: "Oct 6, 2013",
+    channelViews: "3,314,271",
+    views: "215k views",
+    subscribers: "86.5k",
+    channelImg: "AKedOLT0qFcyVvI-0xCjOfjPq6Ac15ZrVnINc3U3FrXrFw",
+    category: 'Naval'
   },
   {
     _id: "MPwWLBIywrw",
@@ -65,19 +65,19 @@
     category: 'PowerfulJRE'
   },
   {
-    _id: "g87ZSTomZDY",
-    title: "Vitalik: Ethereum, Part 1",
+    _id: "KRvv0QdruMQ",
+    title: "Are There Lost Alien Civilizations in Our Past?",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    channel: "Naval",
-    timesAgo: "5 hours ago",
-    timeLength: "0:16",
-    channelJoined: "Oct 6, 2013",
-    channelViews: "3,314,271",
-    views: "215k views",
-    subscribers: "86.5k",
-    channelImg: "AKedOLT0qFcyVvI-0xCjOfjPq6Ac15ZrVnINc3U3FrXrFw",
-    category: 'Naval'
+      "When we think about alien civilizations we tend to look into the vastness of space, to far away planets. But there is another incredibly vast dimension that we might be giving too little thought to: time.",
+    channel: "Kurzgesagt",
+    timesAgo: "Mar 1, 2022",
+    timeLength: "11:56",
+    channelJoined: "Jul 10, 2013",
+    channelViews: "1,265,142",
+    views: "8.7M views",
+    subscribers: "18.5M",
+    channelImg: "AKedOLRVT94BfVl1Xw1UCqakso5v7sSWB0QQvLd3fAAKTw",
+    category: 'Kurzgesagt'
   },
   {
     _id: "CdgjGykrByM",
@@ -110,34 +110,34 @@
     category: 'Fireship'
   },
   {
-    _id: "g-_hVXzkn0o",
-    title: "Is the tech bubble bursting right now?",
+    _id: "3qHkcs3kG44",
+    title: "Joe Rogan Experience #1309 - Naval Ravikant",
     description:
-      "Tech companies have seen huge declines in value over the past six months... What does this mean for programmers and entrepreneurs? Let's look at bad news affecting tech workers and how it relates to coding jobs, venture capital, crypto, and startups.",
-    channel: "Fireship",
-    timesAgo: "Jan 17, 2022",
-    timeLength: "4:16",
-    channelJoined: "Nov 17, 2019",
-    channelViews: "265,142",
-    views: "9.4k views",
-    subscribers: "597K",
-    channelImg: "AKedOLRVT94BfVl1Xw1UCqakso5v7sSWB0QQvLd3fAAKTw",
-    category: 'Fireship'
+      "Naval Ravikant is an entrepreneur and angel investor, a co-author of Venture Hacks, and a co-maintainer of AngelList.",
+    channel: "PowerfulJRE",
+    timesAgo: "Jan 31, 2018",
+    timeLength: "35:16",
+    channelJoined: "Nov 17, 2013",
+    channelViews: "2, 265,142",
+    views: "12.8M views",
+    subscribers: "12.4M",
+    channelImg: "AKedOLRkQQQepdHNQ30TP5ywVn7paxmZc5MAynPnkzLg6g",
+    category: 'PowerfulJRE'
   },
   {
-    _id: "ZzDxmAcHMOs",
-    title: "How to focus | Andrew Huberman and Lex Fridman",
+    _id: "5Eqb_-j3FDA",
+    title: "Coke Studio | Season 14 | Pasoori | Ali Sethi x Shae Gill",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    channel: "John Doe",
-    timesAgo: "5 hours ago",
-    timeLength: "10:42",
+      "Let’s transcend boundaries and bridge distances through compassion, love and identity.",
+    channel: "Coke Studio",
+    timesAgo: "Feb 7, 2022",
+    timeLength: "3:16",
     channelJoined: "Nov 17, 2019",
     channelViews: "265,142",
     views: "9.4k views",
-    subscribers: "597K",
-    channelImg: "AKedOLQ7TSUR7ber65I8J7QRgzUsEjJ01GitgCUZgcQM",
-    category: 'Lex Clips'
+    subscribers: "11.9M",
+    channelImg: "AKedOLTNtHgVmX114S4cnjoyFaDDJE6N1zNBwKgRNnYNAg",
+    category: 'Music'
   },
   {
     _id: "M2uEym-a-ts",
@@ -155,6 +155,36 @@
     category: 'Lex Clips'
   },
   {
+    _id: "vQcCNpuaJO8",
+    title: "Currying in Javascript | JS Interview Questions",
+    description:
+      "Function Currying is a cool feature of functional programming with Javascript. This video covers two ways to achieve that, firstly by using bind function and the second using the concept of closure. ",
+    channel: "Akshay Saini",
+    timesAgo: "Feb 6, 2019",
+    timeLength: "8:16",
+    channelJoined: "Nov 17, 2019",
+    channelViews: "265,142",
+    views: "9.4k views",
+    subscribers: "597K",
+    channelImg: "AKedOLR8gqN3fHHNYbehMcsJ49rapBPhJMGPYSrl6YQyNg",
+    category: 'Akshay Saini'
+  },
+  {
+    _id: "g-_hVXzkn0o",
+    title: "Is the tech bubble bursting right now?",
+    description:
+      "Tech companies have seen huge declines in value over the past six months... What does this mean for programmers and entrepreneurs? Let's look at bad news affecting tech workers and how it relates to coding jobs, venture capital, crypto, and startups.",
+    channel: "Fireship",
+    timesAgo: "Jan 17, 2022",
+    timeLength: "4:16",
+    channelJoined: "Nov 17, 2019",
+    channelViews: "265,142",
+    views: "9.4k views",
+    subscribers: "597K",
+    channelImg: "AKedOLRVT94BfVl1Xw1UCqakso5v7sSWB0QQvLd3fAAKTw",
+    category: 'Fireship'
+  },
+  {
     _id: "nm1TxQj9IsQ",
     title: "Master Your Sleep & Be More Alert When Awake",
     description:
@@ -170,34 +200,19 @@
     category: 'Andrew Huberman'
   },
   {
-    _id: "ouCWNRvPk20",
-    title: "Dr. Justin Sonnenburg: How to Build, Maintain & Repair Gut Health",
+    _id: "ZzDxmAcHMOs",
+    title: "How to focus | Andrew Huberman and Lex Fridman",
     description:
-      "My guest this episode is Dr. Justin Sonnenburg, Professor of Microbiology & Immunology at Stanford University. Dr. Sonnenburg’s research focuses on how microbes in our gut impact our mental and physical health and how diet and your environment shape your gut microbiome.",
-    channel: "Andrew Huberman",
-    timesAgo: "19 days ago",
-    timeLength: "38:27",
-    channelJoined: "Nov 17, 2019",
-    channelViews: "265,142",
-    views: "27k views",
-    subscribers: "1.05M",
-    channelImg: "AKedOLS6do_PDpVTlBPBExliQcGseqdl3P3HdF6KETERgQ",
-    category: 'Andrew Huberman'
-  },
-  {
-    _id: "szqPAPKE5tQ",
-    title: "Understand & Improve Memory Using Science-Based Tools",
-    description:
-      "This episode I explain the mechanisms by which different types of memories are established in our brain and how to leverage the amount and timing of key neurochemicals and hormones.",
-    channel: "Andrew Huberman",
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    channel: "John Doe",
     timesAgo: "5 hours ago",
-    timeLength: "35:27",
+    timeLength: "10:42",
     channelJoined: "Nov 17, 2019",
     channelViews: "265,142",
     views: "9.4k views",
-    subscribers: "1.05M",
-    channelImg: "AKedOLS6do_PDpVTlBPBExliQcGseqdl3P3HdF6KETERgQ",
-    category: 'Andrew Huberman'
+    subscribers: "597K",
+    channelImg: "AKedOLQ7TSUR7ber65I8J7QRgzUsEjJ01GitgCUZgcQM",
+    category: 'Lex Clips'
   },
   {
     _id: "ZvbzSrg0afE",
@@ -215,79 +230,19 @@
     category: 'Akshay Saini'
   },
   {
-    _id: "vQcCNpuaJO8",
-    title: "Currying in Javascript | JS Interview Questions",
+    _id: "kw4tT7SCmaY",
+    title: "Afreen Afreen| Rahat Fateh Ali Khan & Momina Mustehsan",
     description:
-      "Function Currying is a cool feature of functional programming with Javascript. This video covers two ways to achieve that, firstly by using bind function and the second using the concept of closure. ",
-    channel: "Akshay Saini",
-    timesAgo: "Feb 6, 2019",
-    timeLength: "8:16",
-    channelJoined: "Nov 17, 2019",
-    channelViews: "265,142",
-    views: "9.4k views",
-    subscribers: "597K",
-    channelImg: "AKedOLR8gqN3fHHNYbehMcsJ49rapBPhJMGPYSrl6YQyNg",
-    category: 'Akshay Saini'
-  },
-  {
-    _id: "wSrXcM_5fqc",
-    title: "Are There Lost Alien Civilizations in Our Past?",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    channel: "Varun Mayya",
+      "Nusrat Fateh Ali Khan was one of the shining lights of Pakistan, a Qawwali Shahehshah whose talent was heralded both locally and globally.To celebrate the legend that still lives 19 years on, Coke Studio presents Afreen Afreen, Rahat Fateh Ali Khan & Momina Mustehsan, Episode 2, Coke Studio 9",
+    channel: "Coke Studio",
     timesAgo: "5 hours ago",
     timeLength: "0:16",
     channelJoined: "Nov 17, 2019",
     channelViews: "265,142",
-    views: "94k views",
-    subscribers: "97K",
-    channelImg: "AKedOLSqLOIYDn9ln7gpnkfJDrb61dPkbqnDWfRLLJGzlw",
-    category: 'Varun Mayya'
-  },
-  {
-    _id: "R83OY_N9ZhI",
-    title: "Top 3 Brain Hacking Devices I Used To Control My Brain",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    channel: "Varun Mayya",
-    timesAgo: "5 hours ago",
-    timeLength: "0:16",
-    channelJoined: "Nov 17, 2019",
-    channelViews: "265,142",
-    views: "19.4k views",
-    subscribers: "97K",
-    channelImg: "AKedOLSqLOIYDn9ln7gpnkfJDrb61dPkbqnDWfRLLJGzlw",
-    category: 'Varun Mayya'
-  },
-  {
-    _id: "ZKPQn3BVAYg",
-    title: "Mark Zuckerberg on The Tim Ferriss Show",
-    description:
-      "He is also the co-founder and co-CEO of the Chan Zuckerberg Initiative with his wife Priscilla, which is leveraging technology to help solve some of the world’s toughest challenges—including supporting the science and technology.",
-    channel: "Tim Ferriss",
-    timesAgo: "Oct 29, 2015",
-    timeLength: "53:16",
-    channelJoined: "Nov 17, 2019",
-    channelViews: "265,142",
-    views: "413k views",
-    subscribers: "1.02M",
-    channelImg: "AKedOLSZJdHF2i4pw4h_EZ_60rSMappxs-JQoTjufUuV1A",
-    category: 'Tim Ferriss'
-  },
-  {
-    _id: "EIm4HvDgQCM",
-    title: "Chill Drive - Lofi hip hop mix ~ Stress Relief, Relaxing Music",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    channel: "John Doe",
-    timesAgo: "5 hours ago",
-    timeLength: "0:16",
-    channelJoined: "Nov 17, 2019",
-    channelViews: "265,142",
-    views: "9.4k views",
-    subscribers: "597K",
-    channelImg: "AKedOLRVT94BfVl1Xw1UCqakso5v7sSWB0QQvLd3fAAKTw",
-    category: 'Elon 1'
+    views: "3.4k views",
+    subscribers: "11.9M",
+    channelImg: "AKedOLTNtHgVmX114S4cnjoyFaDDJE6N1zNBwKgRNnYNAg",
+    category: 'Music'
   },
   {
     _id: "tt2k8PGm-TI",
@@ -305,6 +260,51 @@
     category: 'Music'
   },
   {
+    _id: "wSrXcM_5fqc",
+    title: "Are There Lost Alien Civilizations in Our Past?",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    channel: "Varun Mayya",
+    timesAgo: "5 hours ago",
+    timeLength: "0:16",
+    channelJoined: "Nov 17, 2019",
+    channelViews: "265,142",
+    views: "94k views",
+    subscribers: "97K",
+    channelImg: "AKedOLSqLOIYDn9ln7gpnkfJDrb61dPkbqnDWfRLLJGzlw",
+    category: 'Varun Mayya'
+  },
+  {
+    _id: "ouCWNRvPk20",
+    title: "Dr. Justin Sonnenburg: How to Build, Maintain & Repair Gut Health",
+    description:
+      "My guest this episode is Dr. Justin Sonnenburg, Professor of Microbiology & Immunology at Stanford University. Dr. Sonnenburg’s research focuses on how microbes in our gut impact our mental and physical health and how diet and your environment shape your gut microbiome.",
+    channel: "Andrew Huberman",
+    timesAgo: "19 days ago",
+    timeLength: "38:27",
+    channelJoined: "Nov 17, 2019",
+    channelViews: "265,142",
+    views: "27k views",
+    subscribers: "1.05M",
+    channelImg: "AKedOLS6do_PDpVTlBPBExliQcGseqdl3P3HdF6KETERgQ",
+    category: 'Andrew Huberman'
+  },
+  {
+    _id: "ZKPQn3BVAYg",
+    title: "Mark Zuckerberg on The Tim Ferriss Show",
+    description:
+      "He is also the co-founder and co-CEO of the Chan Zuckerberg Initiative with his wife Priscilla, which is leveraging technology to help solve some of the world’s toughest challenges—including supporting the science and technology.",
+    channel: "Tim Ferriss",
+    timesAgo: "Oct 29, 2015",
+    timeLength: "53:16",
+    channelJoined: "Nov 17, 2019",
+    channelViews: "265,142",
+    views: "413k views",
+    subscribers: "1.02M",
+    channelImg: "AKedOLSZJdHF2i4pw4h_EZ_60rSMappxs-JQoTjufUuV1A",
+    category: 'Tim Ferriss'
+  },
+  {
     _id: "udHq9Mg14Os",
     title: "MBNN & Rowald Steyn - ilomilo (Billie Eilish)",
     description:
@@ -317,36 +317,6 @@
     views: "7.2M views",
     subscribers: "119k",
     channelImg: "AKedOLS9cYksjGLCzvxMQfCGDfyq-pPqOv_F6cfqG6wfoQ",
-    category: 'Music'
-  },
-  {
-    _id: "kw4tT7SCmaY",
-    title: "Afreen Afreen| Rahat Fateh Ali Khan & Momina Mustehsan",
-    description:
-      "Nusrat Fateh Ali Khan was one of the shining lights of Pakistan, a Qawwali Shahehshah whose talent was heralded both locally and globally.To celebrate the legend that still lives 19 years on, Coke Studio presents Afreen Afreen, Rahat Fateh Ali Khan & Momina Mustehsan, Episode 2, Coke Studio 9",
-    channel: "Coke Studio",
-    timesAgo: "5 hours ago",
-    timeLength: "0:16",
-    channelJoined: "Nov 17, 2019",
-    channelViews: "265,142",
-    views: "3.4k views",
-    subscribers: "11.9M",
-    channelImg: "AKedOLTNtHgVmX114S4cnjoyFaDDJE6N1zNBwKgRNnYNAg",
-    category: 'Music'
-  },
-  {
-    _id: "5Eqb_-j3FDA",
-    title: "Coke Studio | Season 14 | Pasoori | Ali Sethi x Shae Gill",
-    description:
-      "Let’s transcend boundaries and bridge distances through compassion, love and identity.",
-    channel: "Coke Studio",
-    timesAgo: "Feb 7, 2022",
-    timeLength: "3:16",
-    channelJoined: "Nov 17, 2019",
-    channelViews: "265,142",
-    views: "9.4k views",
-    subscribers: "11.9M",
-    channelImg: "AKedOLTNtHgVmX114S4cnjoyFaDDJE6N1zNBwKgRNnYNAg",
     category: 'Music'
   },
   {
@@ -365,19 +335,34 @@
     category: 'Tim Ferriss'
   },
   {
-    _id: "3qHkcs3kG44",
-    title: "Joe Rogan Experience #1309 - Naval Ravikant",
+    _id: "szqPAPKE5tQ",
+    title: "Understand & Improve Memory Using Science-Based Tools",
     description:
-      "Naval Ravikant is an entrepreneur and angel investor, a co-author of Venture Hacks, and a co-maintainer of AngelList.",
-    channel: "PowerfulJRE",
-    timesAgo: "Jan 31, 2018",
-    timeLength: "35:16",
-    channelJoined: "Nov 17, 2013",
-    channelViews: "2, 265,142",
-    views: "12.8M views",
-    subscribers: "12.4M",
-    channelImg: "AKedOLRkQQQepdHNQ30TP5ywVn7paxmZc5MAynPnkzLg6g",
-    category: 'PowerfulJRE'
+      "This episode I explain the mechanisms by which different types of memories are established in our brain and how to leverage the amount and timing of key neurochemicals and hormones.",
+    channel: "Andrew Huberman",
+    timesAgo: "5 hours ago",
+    timeLength: "35:27",
+    channelJoined: "Nov 17, 2019",
+    channelViews: "265,142",
+    views: "9.4k views",
+    subscribers: "1.05M",
+    channelImg: "AKedOLS6do_PDpVTlBPBExliQcGseqdl3P3HdF6KETERgQ",
+    category: 'Andrew Huberman'
+  },
+  {
+    _id: "R83OY_N9ZhI",
+    title: "Top 3 Brain Hacking Devices I Used To Control My Brain",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    channel: "Varun Mayya",
+    timesAgo: "5 hours ago",
+    timeLength: "0:16",
+    channelJoined: "Nov 17, 2019",
+    channelViews: "265,142",
+    views: "19.4k views",
+    subscribers: "97K",
+    channelImg: "AKedOLSqLOIYDn9ln7gpnkfJDrb61dPkbqnDWfRLLJGzlw",
+    category: 'Varun Mayya'
   },
   {
     _id: "6T7pUEZfgdI",
